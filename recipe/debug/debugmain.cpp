0b4f277f9b6363dfa4fd8fddd7658e5297bb067c
Silvio Traversaro <silvio@traversaro.it>
2022-11-25 23:22:19 +0100

    Create debugmain.cpp

---
+++ recipe/debug/debugmain.cpp
@@ -0,0 +1,84 @@
+
+#if defined(__ppc__)
+#warning "__ppc__ is defined."
+#else
+#warning "__ppc__ is not defined."
+#endif
+
+
+#if defined(__ppc64__)
+#warning "__ppc64__ is defined."
+#else
+#warning "__ppc64__ is not defined."
+#endif
+
+#if defined(__powerpc)
+#warning "__powerpc is defined."
+#else
+#warning "__powerpc is not defined."
+#endif
+
+#if defined(__powerpc64__)
+#warning "__powerpc64__ is defined."
+#else
+#warning "__powerpc64__ is not defined."
+#endif
+
+#if defined(__POWERPC__)
+#warning "__POWERPC__ is defined."
+#else
+#warning "__POWERPC__ is not defined."
+#endif
+
+#if defined(__PPC__)
+#warning "__PPC__ is defined."
+#else
+#warning "__PPC__ is not defined."
+#endif
+
+#if defined(__PPC64__)
+#warning "__PPC64__ is defined."
+#else
+#warning "__PPC64__ is not defined."
+#endif
+
+#if defined(_ARCH_PPC)
+#warning "_ARCH_PPC is defined."
+#else
+#warning "_ARCH_PPC is not defined."
+#endif
+  
+#if defined(_ARCH_PPC64)
+#warning "_ARCH_PPC64 is defined."
+#else
+#warning "_ARCH_PPC64 is not defined."
+#endif
+
+#if defined(__amd64__)
+#warning "__amd64__ is defined."
+#else
+#warning "__amd64__ is not defined."
+#endif
+
+#if defined(__amd64)
+#warning "__amd64 is defined."
+#else
+#warning "__amd64 is not defined."
+#endif
+
+#if defined(__x86_64__)
+#warning "__x86_64__ is defined."
+#else
+#warning "__x86_64__ is not defined."
+#endif
+  
+#if defined(__x86_64)
+#warning "__x86_64 is defined."
+#else
+#warning "__x86_64 is not defined."
+#endif
+
+int func()
+{
+  return 2;
+}
