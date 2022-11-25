01a37154c590f0deeddb4e40d825c3369f21d4ea
Silvio Traversaro <silvio@traversaro.it>
2022-11-25 23:25:05 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,6 +21,17 @@ else
   export OGRE_SIMD_NEON=OFF
 fi
 
+cd debug
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS:BOOL=ON
+cmake --build . --config Release --parallel ${CPU_COUNT}
+cd ../..
+
+
 rm -rf build
 mkdir build
 cd build
